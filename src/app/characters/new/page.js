4612dc110f3b40d6c6b6c691bf2ff90a2aed09ca
Abigail Olaguivel <abbyog27@gmail.com>
2023-12-23 16:38:47 -0600

    feat: added create characters page

---
+++ src/app/characters/new/page.js
@@ -0,0 +1,9 @@
+"use client"
+import { useEffect, useState } from "react";
+import SignUp from "@/app/components/SignUp";
+
+export default function CreateCharacter() {
+    return (
+      <SignUp />
+    )
+  }
