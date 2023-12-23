11e919c25db24e2fd99964e8b0ae562557596461
Rodrigo <velez59@gmail.com>
2023-12-23 14:15:31 -0800

    feat: added Login Page

---
+++ src/app/auth/page.js
@@ -0,0 +1,9 @@
+"use client"
+import { useEffect, useState } from "react";
+import SignInSide from "../components/SignInSide";
+
+export default function Blog() {
+    return (
+      <SignInSide />
+    )
+  }
