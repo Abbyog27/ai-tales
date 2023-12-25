ced20b81e82feae3318214c09ea1e781c950e362
Abigail Olaguivel <abbyog27@gmail.com>
2023-12-25 15:47:19 -0600

    feat: added EditCharacter component and created view page for edit character

---
+++ src/app/characters/edit/page.js
@@ -0,0 +1,9 @@
+"use client"
+import { useEffect, useState } from "react";
+import EditCharacter from "@/app/components/Edit Character";
+
+export default function Edit() {
+    return (
+      <EditCharacter />
+    )
+  }
---
+++ src/app/components/Edit Character.js
@@ -0,0 +1,112 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function EditCharacter() {
+  
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="species"
+              label="Species"
+              name="species"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="gender"
+              label="Gender"
+              name="gender"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="age"
+              label="Age"
+              name="age"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="personality"
+              label="Personality"
+              name="personality"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="hobby"
+              label="Favorite Hobby"
+              name="hobby"
+              autoFocus
+            />
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Edit Character
+            </Button>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              color="error"
+            >
+              Cancel
+            </Button>
+            
+          </Box>
+        </Box>
+    
+      </Container>
+    </ThemeProvider>
+  );
+        }
+      
