d3c5530509551c5a4e7802da8457955800f13029
Abigail Olaguivel <abbyog27@gmail.com>
2023-12-26 00:08:51 -0600

    feat: added ViewCharacter component and created view page for view character

---
+++ src/app/characters/view/page.js
@@ -0,0 +1,9 @@
+"use client"
+import { useEffect, useState } from "react";
+import ViewCharacter from "@/app/components/ViewCharacter";
+
+export default function View() {
+    return (
+      <ViewCharacter />
+    )
+  }
