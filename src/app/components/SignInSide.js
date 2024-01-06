ecd1357cc2acf21776951508cbfdc37b4319b1a9
Rodrigo Vélez <Velez59@gmail.com>
2024-01-06 14:02:27 -0800

    chore: added branding to signin

--- src/app/components/SignInSide.js
+++ src/app/components/SignInSide.js
@@ -84,7 +84,7 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundImage: 'url(https://github.com/soyrvelez/ai-tales/raw/main/ai-tales.png?sanitize=true)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
